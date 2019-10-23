0661c3746edb1099e794351173c3b47ffdc8d317
teg-lad <56928797+teg-lad@users.noreply.github.com>
2019-10-23 17:28:05 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -15,6 +15,18 @@ def main():
 	x = square(15)
 	print(x)
 
+def fact(x):
+   total = 1
+
+   while x != 0:
+      total = total * x
+      if x > 0:
+         x = x - 1
+      elif x < 0:
+         x = x + 1
+   return total
+
+
 
 if __name__ == "__main__":
   print("test")
