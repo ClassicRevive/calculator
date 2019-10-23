0d4f37a18d4d2483c2cf7951373ef5579e75f4f1
Jack Liston <34586111+jackliston1998@users.noreply.github.com>
2019-10-23 16:27:21 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,1 @@
-# put stuff here bro
 
-def divide(num1, num2):
-    return num1 / float(num2)
-
-#put shit here bro
-
-def cubed(number):
-    return number ** 3
-#!/usr/bin/env python
-
-def main():
-    print "-----------------------"
-    print "Hacktobercub calculator"
-    print "-----------------------"
-    print ""
-
-if __name__ == "__main__":
-    main()
